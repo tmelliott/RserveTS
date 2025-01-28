6ea0b67993ba981e1e06d2d98286e2f451bd79fc
Tom Elliott <tomelliottnz@gmail.com>
2025-01-29 09:29:42 +1300

    add file

---
+++ vignettes/faithful-app.R
@@ -0,0 +1,16 @@
+library(ts)
+
+get_hist <- ts_function(
+    function(bins = ts_integer(1)) {
+        h <- hist(faithful$waiting, breaks = bins, plot = FALSE)
+        data.frame(x = h$mids, y = h$density)
+    },
+    result = ts_dataframe(eruptions = ts_numeric(), waiting = ts_numeric())
+)
+get_smoother <- ts_function(
+    function(bandwidth = ts_numeric(1)) {
+        d <- density(faithful$waiting, bw = bandwidth)
+        data.frame(x = d$x, y = d$y)
+    },
+    result = ts_dataframe(x = ts_numeric(), y = ts_numeric())
+)
